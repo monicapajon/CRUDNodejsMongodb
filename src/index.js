42fae24a474f17258538a08815c68df6d0eae74f
Monica Pajon <moniestudio161@gmail.com>
2022-09-10 18:33:50 -0300

    se crearon carpetas y archivos de la estructura

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
-import express from "express";
-
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Hello world");
-});
+import app from "./app";
 app.listen(3000);
 console.log("Server on port", 3000);

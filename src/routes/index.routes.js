599eaa8838481fe75e18504d2f0f883da714149a
Monica Pajon <moniestudio161@gmail.com>
2022-09-11 18:32:30 -0300

    Formulario y ruta POST

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,18 +1,24 @@
 import { Router } from "express";
+import Task from "../models/Task";
+
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.render('index');
+  res.render("index");
+});
+
+router.post("/tasks/add", (req, res) => {
+  const task = Task(req.body);
+  //console.log(task);
+  res.send("saved");
 });
 
 router.get("/about", (req, res) => {
-    res.render('about');
-  });
+  res.render("about");
+});
 
-  router.get("/edit", (req, res) => {
-    res.render('edit');
-  });
-
-
+router.get("/edit", (req, res) => {
+  res.render("edit");
+});
 
 export default router;

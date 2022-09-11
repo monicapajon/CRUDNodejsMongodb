77b8bd2f02902a71a6ecdd78a60bbde784f40fcf
Monica Pajon <moniestudio161@gmail.com>
2022-09-10 21:17:28 -0300

    Handlebars

--- src/app.js
+++ src/app.js
@@ -1,8 +1,50 @@
-import express from "express";
+// import express from "express";
+// import exphbs from "express-handlebars";
+// import indexRoutes from "./routes/index.routes";
+
+// const hbs = create({
+//     layoutsDir: path.join(app.get("views"), 'layouts'),
+//     defaultLayout: 'main',
+//     extname: '.hbs',
+// });
+
+// const app = express();
+
+
+// app.set('views', './views');
+
+// app.engine(".hbs",hbs.engine({
+//     extname:".hbs",
+// }));
+
+// //Routes
+// app.use(indexRoutes);
+
+// export default app;
+
+import express from 'express';
 import indexRoutes from './routes/index.routes';
+import { create } from 'express-handlebars';
+import path from 'path';
 
 const app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+
+app.engine(
+    '.hbs', 
+    create({
+        layoutsDir: path.join(app.get('views'), 'layouts'),
+        partialsDir: path.join(app.get('views'), 'partials'),
+        defaultLayout: 'main',
+        extname: '.hbs',
+    }).engine
+);
+
+
+app.set('view engine', '.hbs');
+
+//Routes
 app.use(indexRoutes);
 
 export default app;
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,7 +2,17 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.send("Hello world");
+  res.render('index');
 });
+
+router.get("/about", (req, res) => {
+    res.render('about');
+  });
+
+  router.get("/edit", (req, res) => {
+    res.render('edit');
+  });
+
+
 
 export default router;

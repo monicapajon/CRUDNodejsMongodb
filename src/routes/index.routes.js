d95e8cae31cd46ad1a2ec99b3590f9bedbbdc3c0
Monica Pajon <moniestudio161@gmail.com>
2022-09-12 10:53:34 -0300

    listar y guardar datos

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,8 +3,10 @@ import Task from "../models/Task";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.render("index");
+router.get("/", async (req, res) => {
+  const tasks = await Task.find().lean();
+  //console.log(tasks);
+  res.render("index", { tasks: tasks });
 });
 
 router.post("/tasks/add", async (req, res) => {
@@ -24,4 +26,3 @@ router.get("/edit", (req, res) => {
 });
 
 export default router;
-

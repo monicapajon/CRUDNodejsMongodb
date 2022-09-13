5143aabe51373db42d89e0d89f06ddc7399a5ea4
Monica Pajon <moniestudio161@gmail.com>
2022-09-13 15:03:40 -0300

    Eliminando tareas

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -43,4 +43,10 @@ router.post("/edit/:id", async (req, res) => {
   res.redirect("/");
 });
 
+router.get("/delete/:id", async (req, res) => {
+  const { id } = req.params;
+  await Task.findByIdAndDelete(id);
+  res.redirect("/");
+});
+
 export default router;

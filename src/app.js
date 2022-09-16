04a8af51347aeecab6b1e40336819be003c2744a
Monica Pajon <moniestudio161@gmail.com>
2022-09-16 16:06:50 -0300

    css-icons

--- src/app.js
+++ src/app.js
@@ -27,4 +27,7 @@ app.use(express.urlencoded({ extended: false }));
 //Routes
 app.use(indexRoutes);
 
+//Static files
+app.use(express.static(path.join(__dirname, "public")));
+
 export default app;
